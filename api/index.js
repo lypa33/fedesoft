405d4c09b4476eb4667e3de99239e4bfe3bab9a2
Eddie Ruiz <dev.blacknode@gmail.com>
2018-08-08 15:10:20 -0500

    Building API REST

--- api/index.js
+++ api/index.js
@@ -1,23 +1,40 @@
-const rest = require('express')
-
+const rest      = require('express')
+const base64    = require('js-base64').Base64
+const logger    = require('./libs/logger')
+const make_string   = require('sprintf-js').sprintf
 
 const app = rest()
 
 app.get('/', (req, res) => {
+    res.status(301)
+    res.sendFile('views/hello_world.html', { root: __dirname})
+})
+
+app.get('/users/:nickname', (req, res) => {
 
     if (typeof req.query.key === 'undefined')
     {
         res.status(511)
-        res.json({ "response": 'Network Authentication Required'})
+        res.sendFile('views/access_denied.html', { root: __dirname})
+        logger('API Key is undefined (not provided)')
         return
     }
 
-    console.log(req.query.key)
+    logger(make_string("API Key: %s", req.query.key))
+    
+    logger(make_string("User: %s", req.params.nickname))
+
+    const str = base64.encode(req.query.key)
+
     res.status(200)
-    res.send('OK')
+    res.send('OK '+str)
 
 })
 
 app.listen(3002, () => {
-    console.log('Server running')
+    logger('Server running')
+    logger(make_string('Platform: %s', process.platform))
+    logger(make_string('Arch: %s', process.arch))
+    logger(make_string('App path: %s', process.cwd()))
+    logger(make_string('PID: %s', process.pid))
 })
